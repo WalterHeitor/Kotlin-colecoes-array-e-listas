4d682dbed177de58fd407cece047c27c90e47d79
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-03-11 12:24:37 -0300

    funcoes arrays

--- ColecoesListasArray/src/main/kotlin/main.kt
+++ ColecoesListasArray/src/main/kotlin/main.kt
@@ -1,10 +1,37 @@
 package br.com.alura
 
-import java.math.BigDecimal
-
 fun main(){
 
+    val serie: IntRange = 1.rangeTo(10)
+    for( s in serie ){
+        print("$s, ")
+    }
+    println()
+    val numerospares = 0..20 step 2
+    for(n in numerospares){
+        print("$n ")
+    }
 
+    println()
+    val numerospares2 = 2.until(30) step 2
+    for(n in numerospares2){
+        print("$n ")
+    }
+
+    println()
+    val intProgression = 30 downTo 0 step 2
+    for(n in intProgression){
+        print("$n ")
+    }
+    println()
+    val intervalo = 1500.0..5000.0
+    val s = 4000.0
+    if (s in intervalo){
+        println("esta dentro do intervalo")
+    }else{
+        println("nao esta no intervalo")
+    }
+    'a'
 
 }
 
